a58f3d0b0aeea51816b9758cfbb72bb2d0638ed5
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-08-12 14:36:34 +0300

    Update MOD-10_module_10_4.py

--- MOD-10_module_10_4.py
+++ MOD-10_module_10_4.py
@@ -49,20 +49,20 @@ class Cafe:
                         guest.start()
                         print(f"{guest.name} вышел из очереди и сел за стол номер {table.number}.")
 
-# Создание столов
+
 tables = [Table(number) for number in range(1, 6)]
-# Имена гостей
+
 guests_names = [
 'Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman',
 'Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra'
 ]
-# Создание гостей
+
 guests = [Guest(name) for name in guests_names]
-# Заполнение кафе столами
+
 cafe = Cafe(*tables)
-# Приём гостей
+
 cafe.guest_arrival(*guests)
-# Обслуживание гостей
+
 cafe.discuss_guests()
 
 
